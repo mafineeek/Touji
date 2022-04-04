315a931fd94bde14dedce15abc31bbeb3d13c93f
Kamil G <xmuffionex.dev@ogarnij.se>
2022-04-04 20:42:16 +0200

    disable beta

--- data/Config.ts
+++ data/Config.ts
@@ -1,5 +1,5 @@
 export default class Config {
-  static BETA = true;
+  static BETA = false;
   static BETA_TOKEN =
     "OTU4NDU1MDI2OTA3NDk2NTI5.YkNkzQ.lmjWvywUAU8HIW44xKO8yJvghp0";
   static TOKEN = "OTU5NTYxMzc2NDIwOTkxMDM3.YkdrKw.eAJCBc9uq_aeepJYA4EMJ5HEPA0";
