7c06c5982ae8f9427503ede1a394d2d6a7e34175
Kamil G <xmuffionex.dev@ogarnij.se>
2022-04-09 13:50:06 +0200

    refactor: rewrite LocalisationService

--- index.ts
+++ index.ts
@@ -5,18 +5,24 @@ import Bot from "./util/Bot";
 global.LanguageHandler = LanguageHandler;
 
 const bot = new Bot({
-    intents: [
-        Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_BANS,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
-    ],
-    partials: ['USER', 'REACTION', 'MESSAGE']
-})
-bot.setup()
-process.on("unhandledRejection", (error: Error) => {
-  bot.errorWebhook.send(`Unhandled Rejection: \`\`\`\n${error}\n${error.stack}\n\`\`\``)
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_BANS,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
+  partials: ["USER", "REACTION", "MESSAGE"],
 });
-bot.on('error', (error) => {
-    bot.errorWebhook.send(`Error: ${error}`)
-})
+bot.setup();
+process.on("unhandledRejection", (error: Error) => {
+  bot.logger.error(error.stack as string);
+  bot.errorWebhook.send(
+    `Unhandled Rejection: \`\`\`\n${error}\n${error.stack}\n\`\`\``
+  );
+});
+bot.on("error", (error: Error) => {
+  bot.logger.error(error.stack as string);
+  bot.errorWebhook.send(`Error: ${error}`);
+});
 export const client = bot;
--- util/language/LanguageHandler.ts
+++ util/language/LanguageHandler.ts
@@ -1,194 +1,229 @@
 import { BooleanLike, Language } from "../../types";
 
 export default class LanguageHandler {
-    public constructor(
-        private language: Language | "index"
-    ) {}
+  public constructor(private language: Language | "index") {}
 
-    public static get(scope: string, variables: Array<{ old: string, new: any }>, language: Language) {
-        let raw = this.raw(language).propertiesLower();
-        for (const part of scope.split("_")) {
-            if (!raw.hasOwnProperty(part.toLowerCase())) return scope;
-            raw = raw[part.toLowerCase()]
-        }
-
-        if (variables) for (const variable of variables) {
-            raw = raw.replace(`{{${variable.old}}}`, variable.new ?? "Unknown")
-        }
-
-        return raw as any;
+  public static get(
+    scope: string,
+    variables: Array<{ old: string; new: any }>,
+    language: Language
+  ) {
+    let raw = this.raw(language).propertiesLower();
+    for (const part of scope.split("_")) {
+      if (!raw.hasOwnProperty(part.toLowerCase())) return scope;
+      raw = raw[part.toLowerCase()];
     }
 
-    public static getGlobal(scope: string, variables: Array<{ old: string, new: any }>, language: Language) {
-        let raw = this.rawGlobal(language).propertiesLower();
-        for (const part of scope.split("_")) {
-            if (!raw.hasOwnProperty(part.toLowerCase())) return scope;
-            raw = raw[part.toLowerCase()]
-        }
+    if (variables)
+      for (const variable of variables) {
+        raw = raw.replace(`{{${variable.old}}}`, variable.new ?? "Unknown");
+      }
 
-        if (variables) for (const variable of variables) {
-            raw = raw.replace(`{{${variable.old}}}`, variable.new ?? "Unknown")
-        }
+    return raw as any;
+  }
 
-        return raw
+  public static getGlobal(
+    scope: string,
+    variables: Array<{ old: string; new: any }>,
+    language: Language
+  ) {
+    let raw = this.rawGlobal(language).propertiesLower();
+    for (const part of scope.split("_")) {
+      if (!raw.hasOwnProperty(part.toLowerCase())) return scope;
+      raw = raw[part.toLowerCase()];
     }
 
-    public static getStatic(scope: string, variables: Array<{ old: string, new: any }>) {
-        let raw = this.rawGlobal("index").propertiesLower();
-        for (const part of scope.split("_")) {
-            if (!raw.hasOwnProperty(part.toLowerCase())) return scope;
-            raw = raw[part.toLowerCase()]
+    if (variables)
+      for (const variable of variables) {
+        raw = raw.replace(`{{${variable.old}}}`, variable.new ?? "Unknown");
+      }
+
+    return raw;
+  }
+
+  public static getStatic(
+    scope: string,
+    variables: Array<{ old: string; new: any }>
+  ) {
+    let raw = this.rawGlobal("index").propertiesLower();
+    for (const part of scope.split("_")) {
+      if (!raw.hasOwnProperty(part.toLowerCase())) return scope;
+      raw = raw[part.toLowerCase()];
+    }
+
+    if (variables)
+      for (const variable of variables) {
+        raw = raw.replace(`{{${variable.old}}}`, variable.new ?? "Unknown");
+      }
+
+    return raw;
+  }
+
+  public get(scope: string, variables?: Array<{ old: string; new: any }>) {
+    let raw = this.raw().propertiesLower();
+    for (const part of scope.split("_")) {
+      if (!raw.hasOwnProperty(part.toLowerCase())) return scope;
+      raw = raw[part.toLowerCase()];
+    }
+
+    if (variables)
+      for (const variable of variables) {
+        raw = raw.replace(
+          `{{${variable.old}}}`,
+          variable.new ?? this.resolveUnknown()
+        );
+      }
+
+    return raw as any;
+  }
+
+  public setLanguage(language: Language | "index") {
+    this.language = language || "en";
+    return this;
+  }
+
+  public resolveBoolean(boolean: BooleanLike) {
+    switch (this.language) {
+      case "pl": {
+        switch (boolean) {
+          case "yes":
+          case "tak":
+          case "ja":
+          case "true":
+            return "Tak";
+          case "no":
+          case "false":
+          case "nein":
+          case "nie":
+          default:
+            return "Nie";
         }
-
-        if (variables) for (const variable of variables) {
-            raw = raw.replace(`{{${variable.old}}}`, variable.new ?? "Unknown")
+      }
+      case "en": {
+        switch (boolean) {
+          case "yes":
+          case "tak":
+          case "ja":
+          case "true":
+            return "Yes";
+          case "no":
+          case "false":
+          case "nein":
+          case "nie":
+          default:
+            return "No";
         }
-
-        return raw
+      }
     }
+  }
 
-    public get(scope: string, variables?: Array<{ old: string, new: any }>) {
-        let raw = this.raw().propertiesLower();
-        for (const part of scope.split("_")) {
-            if (!raw.hasOwnProperty(part.toLowerCase())) return scope;
-            raw = raw[part.toLowerCase()]
+  public resolveUnknown() {
+    switch (this.language) {
+      case "pl":
+        return "Brak danych";
+      case "en":
+        return "Unknown";
+      default:
+        return "Unknown";
+    }
+  }
+
+  public resolveEnabled(boolean: BooleanLike, suffix?: string) {
+    switch (this.language) {
+      case "pl": {
+        switch (boolean) {
+          case "yes":
+          case "tak":
+          case "ja":
+          case "true":
+            return `Włączon${suffix ?? "y"}` as string;
+          case "no":
+          case "false":
+          case "nein":
+          case "nie":
+          default:
+            return `Wyłączon${suffix ?? "y"}` as string;
         }
-
-        if (variables) for (const variable of variables) {
-            raw = raw.replace(`{{${variable.old}}}`, variable.new ?? this.resolveUnknown())
+      }
+      case "en": {
+        switch (boolean) {
+          case "yes":
+          case "tak":
+          case "ja":
+          case "true":
+            return "Enabled" as string;
+          case "no":
+          case "false":
+          case "nein":
+          case "nie":
+          default:
+            return "Disabled" as string;
         }
+      }
+    }
+  }
 
-        return raw as any;
+  public getGlobal(
+    scope: string,
+    variables?: Array<{ old: string; new: any }>
+  ) {
+    let raw = this.rawGlobal().propertiesLower();
+    for (const part of scope.split("_")) {
+      if (!raw.hasOwnProperty(part.toLowerCase())) return scope;
+      raw = raw[part.toLowerCase()];
     }
 
-    public setLanguage(language: Language | "index") {
-        this.language = language || "en";
-        return this;
+    if (variables)
+      for (const variable of variables) {
+        raw = raw.replace(
+          `{{${variable.old}}}`,
+          variable.new ?? this.resolveUnknown()
+        );
+      }
+
+    return raw;
+  }
+
+  public getStatic(
+    scope: string,
+    variables?: Array<{ old: string; new: any }>
+  ) {
+    let raw = 
+      this.setLanguage("index").rawGlobal()
+    .propertiesLower();
+    for (const part of scope.split("_")) {
+      if (!raw.hasOwnProperty(part.toLowerCase())) return scope;
+      raw = raw[part.toLowerCase()];
     }
 
-    public resolveBoolean(boolean: BooleanLike) {
-        switch (this.language) {
-            case "pl": {
-                switch (boolean) {
-                    case "yes":
-                    case "tak":
-                    case "ja":
-                    case "true":
-                        return "Tak";
-                    case "no":
-                    case "false":
-                    case "nein":
-                    case "nie":
-                    default:
-                        return "Nie";
-                }
-            }
-            case "en": {
-                switch (boolean) {
-                    case "yes":
-                    case "tak":
-                    case "ja":
-                    case "true":
-                        return "Yes";
-                    case "no":
-                    case "false":
-                    case "nein":
-                    case "nie":
-                    default:
-                        return "No";
-                }
-            }
-        }
-    }
+    if (variables)
+      for (const variable of variables) {
+        raw = raw.replace(
+          `{{${variable.old}}}`,
+          variable.new ?? this.resolveUnknown()
+        );
+      }
 
-    public resolveUnknown() {
-        switch (this.language) {
-            case "pl": return "Brak danych"
-            case "en": return "Unknown"
-            default: return "Unknown"
-        }
-    }
+    return raw;
+  }
 
-    public resolveEnabled(boolean: BooleanLike, suffix?: string) {
-        switch (this.language) {
-            case "pl": {
-                switch (boolean) {
-                    case "yes":
-                    case "tak":
-                    case "ja":
-                    case "true":
-                        return `Włączon${suffix ?? "y"}` as string;
-                    case "no":
-                    case "false":
-                    case "nein":
-                    case "nie":
-                    default:
-                        return `Wyłączon${suffix ?? "y"}` as string;
-                }
-            }
-            case "en": {
-                switch (boolean) {
-                    case "yes":
-                    case "tak":
-                    case "ja":
-                    case "true":
-                        return "Enabled" as string;
-                    case "no":
-                    case "false":
-                    case "nein":
-                    case "nie":
-                    default:
-                        return "Disabled" as string;
-                }
-            }
-        }
-    }
+  public raw() {
+    const data = require(`../../data/language/${this.language}.json`);
+    return data
+  }
 
-    public getGlobal(scope: string, variables?: Array<{ old: string, new: any }>) {
-        let raw = this.rawGlobal().propertiesLower();
-        for (const part of scope.split("_")) {
-            if (!raw.hasOwnProperty(part.toLowerCase())) return scope;
-            raw = raw[part.toLowerCase()]
-        }
+  public rawGlobal() {
+    const data = require(`../../data/language/global/${this.language}.json`);
+    return data;
+  }
 
-        if (variables) for (const variable of variables) {
-            raw = raw.replace(`{{${variable.old}}}`, variable.new ?? this.resolveUnknown())
-        }
+  static raw(language: Language) {
+    const data = require(`../../data/language/${language}.json`);
+    return data;
+  }
 
-        return raw
-    }
-
-    public getStatic(scope: string, variables?: Array<{ old: string, new: any }>) {
-        let raw = this.setLanguage("index").rawGlobal().propertiesLower();
-        for (const part of scope.split("_")) {
-            if (!raw.hasOwnProperty(part.toLowerCase())) return scope;
-            raw = raw[part.toLowerCase()]
-        }
-
-        if (variables) for (const variable of variables) {
-            raw = raw.replace(`{{${variable.old}}}`, variable.new ?? this.resolveUnknown())
-        }
-
-        return raw
-    }
-
-    public raw() {
-        const { default: data } = require(`../../data/language/${this.language}`);
-        return data
-    }
-
-    public rawGlobal() {
-        const { default: data } = require(`../../data/language/global/${this.language}`);
-        return data
-    }
-
-    static raw(language: Language) {
-        const { default: data } = require(`../../data/language/${language}`);
-        return data
-    }
-
-    static rawGlobal(language: Language | "index") {
-        const { default: data } = require(`../../data/language/global/${language}`);
-        return data
-    }
+  static rawGlobal(language: Language | "index") {
+    const data = require(`../../data/language/global/${language}.json`);
+    return data;
+  }
 }
